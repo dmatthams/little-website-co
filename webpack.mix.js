dd252a154b501f0e359a6ca8e990cf63e4782487
David Matthams <d.matthams@icloud.com>
2022-09-26 16:06:39 +0000

    Coooking

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,18 @@
 let mix = require('laravel-mix');
 
 mix.js('src/home.js', 'dist').vue();
+mix.css('src/home.css', 'dist');
 
 mix.browserSync({
 	proxy: 'https://little-website-co.webflow.io',
 	files: "./src/",
 	serveStatic: ['.'],
-	// rewriteRules: [
-	//   {
-	// 	match: new RegExp('https://uploads-ssl.webflow.com/5f11a16f2e41f9b9380c2796/css/dmatthams.webflow.26d0ca22c.min.css'),
-	// 	fn: function() {
-	// 	  return '/app.css';
-	// 	}
-	//   }
-	// ]
+	rewriteRules: [
+	  {
+		match: new RegExp(/https:\/\/dmatthams.github.io\/little-website-co\//g),
+		fn: function() {
+		  return '/dist/';
+		}
+	  }
+	]
 });
