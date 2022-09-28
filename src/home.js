0363530ae8242a6b55201038baab7cf521463731
David Matthams <d.matthams@icloud.com>
2022-09-28 13:26:29 +0000

    home assets

--- src/home.js
+++ src/home.js
@@ -1,1 +1,32 @@
-alert('hi dude')
+$( document ).ready(function() {
+
+	const imageSwiper = new Swiper('.featured-work_images', {
+	// Optional parameters
+	effect: 'cards',
+	cardsEffect: {
+	  //rotate: false
+		 },
+  });
+  
+  const textSwiper = new Swiper('.featured-work_text', {
+	// Optional parameters
+	effect: 'fade',
+	initialSlide: 3,
+	rewind: true,
+	slidesPerView:1,
+	autoHeight: true,
+	navigation: {
+		nextEl: '#next',
+	  prevEl: '#back'
+	  },
+		controller: {
+		control: imageSwiper,
+	  },
+	allowTouchMove: false
+  });
+  
+  
+  imageSwiper.on('touchEnd', function () {
+		 textSwiper.slideToLoop(imageSwiper.realIndex);
+	});
+});
